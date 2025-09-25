60e39636fdc860d57086e67201c79cbeeaeb9b00
Alejandra Guillen <alejandraguillen271@gmail.com>
2025-09-25 16:25:39 -0600

    ReservationsDto terminado

---
+++ FlyReservations/DTO/MostrarReservationDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FlyReservations.DTO
+{
+    public record MostrarReservationDto
+        (
+        int Id,
+        int UserId,
+        int FlightId,
+        int SeatId,
+        DateTime Date,
+        string QrCode,
+        string Status
+
+        );
+    
+}
